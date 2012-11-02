fcfcbf28fe992559732f20ab5f728cdd9bdfb993
Benjamin Klum <benjamin.klum@helgoboss.org>
2012-11-02 15:49:47 +0100

    Initial import

---
+++ iota-rivets-observable-adapter.js
@@ -0,0 +1,21 @@
+define(["rivets"], function(rivets) {
+  rivets.configure({
+    adapter: {
+      subscribe: function(obj, keypath, callback) {
+        obj.on(keypath, callback);
+      },
+
+      unsubscribe: function(obj, keypath, callback) {
+        obj.off(keypath, callback);
+      },
+
+      read: function(obj, keypath) {
+        return obj.get(keypath);
+      },
+
+      publish: function(obj, keypath, value) {
+        obj.set(keypath, value);
+      }
+    }
+  });
+})
